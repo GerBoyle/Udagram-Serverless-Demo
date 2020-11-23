7734e1ab8d8b883e357284ceec62ff90378a3b03
Gerard Boyle <ger_boyle@yahoo.ie>
2020-11-23 17:08:35 +0000

    Test commit to dev and then merge

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-
 module.exports = {
   context: __dirname,
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
